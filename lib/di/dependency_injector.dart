6255dd80eb6e8adcfda0f37456c45085466750df
Emanuele Lamagna <elamagna@bsdsoftware.it>
2025-11-29 16:17:40 +0100

    Moved source folder outside ui

--- lib/di/dependency_injector.dart
+++ lib/di/dependency_injector.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:incisive/ui/source/remote/login_service.dart';
+import 'package:incisive/source/remote/login_service.dart';
 import 'package:pine/di/dependency_injector_helper.dart';
 import 'package:provider/provider.dart';
 
