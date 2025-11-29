511a3c6ff52f82e99752c943f3b60782fafdde92
Emanuele Lamagna <elamagna@bsdsoftware.it>
2025-11-29 16:15:52 +0100

    Updated di file

--- lib/di/dependency_injector.dart
+++ lib/di/dependency_injector.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:incisive/ui/source/remote/login_service.dart';
 import 'package:pine/di/dependency_injector_helper.dart';
+import 'package:provider/provider.dart';
 
 import 'package:provider/single_child_widget.dart';
 
