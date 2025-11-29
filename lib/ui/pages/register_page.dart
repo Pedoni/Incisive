9a249697d3a864b83f154118c544f5571abb036d
Emanuele Lamagna <elamagna@bsdsoftware.it>
2025-11-29 16:48:25 +0100

    Fixed pixel overflow and added login bloc calling in login

--- lib/ui/pages/register_page.dart
+++ lib/ui/pages/register_page.dart
@@ -23,59 +23,72 @@ class _RegisterPageState extends State<RegisterPage> {
   }
 
   Widget _buildRegisterContent() {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        const Expanded(child: SizedBox()),
-        Image.asset("assets/images/logo_brown.png", height: 150),
-        const SizedBox(height: 30),
-        Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 24.0),
-          child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 24.0, vertical: 8.0),
-            child: IntrinsicWidth(
-              child: SingleChildScrollView(
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    const Text(
-                      "REGISTER",
-                      textAlign: TextAlign.start,
-                      style: TextStyle(
-                        fontSize: 24,
-                        fontWeight: FontWeight.w500,
-                        fontFamily: 'Poppins',
-                        color: Color.fromARGB(255, 141, 90, 35),
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        return SingleChildScrollView(
+          child: ConstrainedBox(
+            constraints: BoxConstraints(
+              minHeight: constraints.maxHeight,
+            ),
+            child: IntrinsicHeight(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Expanded(child: SizedBox()),
+                  Image.asset("assets/images/logo_brown.png", height: 150),
+                  const SizedBox(height: 30),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 24.0),
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 24.0, vertical: 8.0),
+                      child: IntrinsicWidth(
+                        child: SingleChildScrollView(
+                          child: Column(
+                            mainAxisSize: MainAxisSize.min,
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: [
+                              const Text(
+                                "REGISTER",
+                                textAlign: TextAlign.start,
+                                style: TextStyle(
+                                  fontSize: 24,
+                                  fontWeight: FontWeight.w500,
+                                  fontFamily: 'Poppins',
+                                  color: Color.fromARGB(255, 141, 90, 35),
+                                ),
+                              ),
+                              const SizedBox(height: 20),
+                              LoginTextField(isTextVisible: true, title: "Email", controller: _usernameController),
+                              const SizedBox(height: 10),
+                              LoginTextField(isTextVisible: false, title: "Password", controller: _passwordController),
+                              const SizedBox(height: 10),
+                              LoginTextField(isTextVisible: false, title: "Confirm password", controller: _passwordController),
+                              const SizedBox(height: 15),
+                              LoginButton(
+                                usernameController: _usernameController,
+                                passwordController: _passwordController,
+                                isLoading: false,
+                                login: () {},
+                                color: Color.fromARGB(255, 141, 90, 35),
+                                title: 'Create account',
+                              ),
+                              const SizedBox(height: 20),
+                            ],
+                          ),
+                        ),
                       ),
                     ),
-                    const SizedBox(height: 20),
-                    LoginTextField(isTextVisible: true, title: "Email", controller: _usernameController),
-                    const SizedBox(height: 10),
-                    LoginTextField(isTextVisible: false, title: "Password", controller: _passwordController),
-                    const SizedBox(height: 10),
-                    LoginTextField(isTextVisible: false, title: "Confirm password", controller: _passwordController),
-                    const SizedBox(height: 15),
-                    LoginButton(
-                      usernameController: _usernameController,
-                      passwordController: _passwordController,
-                      isLoading: false,
-                      login: () {},
-                      color: Color.fromARGB(255, 141, 90, 35),
-                      title: 'Create account',
-                    ),
-                    const SizedBox(height: 20),
-                  ],
-                ),
+                  ),
+                  const Expanded(child: SizedBox()),
+                  Image.asset("assets/images/logo_text.png", height: 15, fit: BoxFit.fitHeight),
+                  const SizedBox(height: 40),
+                ],
               ),
             ),
           ),
-        ),
-        const Expanded(child: SizedBox()),
-        Image.asset("assets/images/logo_text.png", height: 15, fit: BoxFit.fitHeight),
-        const SizedBox(height: 40),
-      ],
+        );
+      },
     );
   }
 
@@ -85,11 +98,21 @@ class _RegisterPageState extends State<RegisterPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(foregroundColor: Color.fromARGB(255, 141, 90, 35)),
-      extendBody: true,
-      extendBodyBehindAppBar: true,
-      body: SizedBox(child: SizedBox(child: _buildMobileLayout(context))),
+    return Container(
+      color: Colors.white,
+      child: SafeArea(
+        child: Scaffold(
+          appBar: AppBar(
+            foregroundColor: Color.fromARGB(255, 141, 90, 35),
+            backgroundColor: Colors.white,
+          ),
+          extendBody: true,
+          resizeToAvoidBottomInset: true,
+          extendBodyBehindAppBar: true,
+          backgroundColor: Colors.white,
+          body: SizedBox(child: SizedBox(child: _buildMobileLayout(context))),
+        ),
+      ),
     );
   }
 
