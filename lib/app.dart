d2549727a62d5032ed8039f2f59f6658eefd74a0
Emanuele Lamagna <elamagna@bsdsoftware.it>
2025-11-29 13:36:53 +0100

    Removed safe area

--- lib/app.dart
+++ lib/app.dart
@@ -10,20 +10,18 @@ class App extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: MaterialApp(
-        title: 'Flutter Demo',
-        debugShowCheckedModeBanner: false,
-        theme: ThemeData(colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple)),
-        home: LoginPage(color: Colors.blue),
-        routes: {
-          LoginPage.routeName: (context) => LoginPage(color: Colors.blue),
-          HomePage.routeName: (context) => const HomePage(),
-          DiaryPage.routeName: (context) => const DiaryPage(),
-          TurntablePage.routeName: (context) => const TurntablePage(),
-          BalconyPage.routeName: (context) => const BalconyPage(),
-        },
-      ),
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple)),
+      home: LoginPage(color: Colors.blue),
+      routes: {
+        LoginPage.routeName: (context) => LoginPage(color: Colors.blue),
+        HomePage.routeName: (context) => const HomePage(),
+        DiaryPage.routeName: (context) => const DiaryPage(),
+        TurntablePage.routeName: (context) => const TurntablePage(),
+        BalconyPage.routeName: (context) => const BalconyPage(),
+      },
     );
   }
 }
