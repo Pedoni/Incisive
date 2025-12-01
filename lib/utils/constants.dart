f64d6fd3e628e2b56447b0819ddf951c74e1f770
Emanuele Lamagna <emanuele.lamagna@studio.unibo.it>
2025-12-01 14:49:32 +0100

    Updated diary mock text in constants

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -1,13 +1,21 @@
 import 'package:incisive/models/diary_model.dart';
-import 'package:intl/intl.dart';
 
 class Constants {
   static DiaryEntry mockedDiaryEntry = DiaryEntry(
     DateTime.now(),
-    "Pensieri del giorno\n\n"
-    "Questa è una pagina di diario di esempio.\n"
-    "La data attuale è:\n\n"
-    "${DateFormat('dd/MM/yyyy').format(DateTime.now())}\n\n"
-    "Puoi aggiungere contenuti personalizzati qui.",
+    "Oggi è stata una giornata strana, piena di piccoli momenti "
+    "in bilico tra stanchezza e gratitudine. Mi sono svegliato un po’ "
+    "più teso del solito, con quella sensazione di avere troppe cose in "
+    "testa e poco spazio per respirare. Però, mentre facevo colazione, ho"
+    " notato un raggio di sole che entrava dalla finestra e per qualche"
+    " motivo mi ha fatto sentire meglio.\n\nDurante la giornata ho cercato"
+    " di restare concentrato, anche se non tutto è andato come volevo. Però"
+    " ho avuto una conversazione carina con una persona che non sentivo da "
+    "un po’. Mi ha fatto riflettere su quanto a volte basti davvero poco per"
+    " sentirsi meno soli.\n\nStasera mi sono preso qualche minuto per me. Ho"
+    " messo un po’ di musica tranquilla e ho provato a lasciare andare quello"
+    " che non posso controllare. Non è semplice, ma ci sto lavorando. Forse"
+    " non è stata una giornata perfetta… ma ci sono stati momenti belli, e"
+    " va bene così.",
   );
 }
