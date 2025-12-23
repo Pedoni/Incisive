6544fd319bed8ca382351625c961693979da141e
Emanuele Lamagna <emanuele.lamagna@studio.unibo.it>
2025-12-23 10:53:41 +0100

    Added user model and correct getter

--- lib/repositories/user_repository.dart
+++ lib/repositories/user_repository.dart
@@ -1,15 +1,15 @@
 import 'package:incisive/log/main_logger.dart';
+import 'package:incisive/models/user_model.dart';
 import 'package:incisive/source/remote/user_service.dart';
-import 'package:supabase_auth_ui/supabase_auth_ui.dart';
 
 class UserRepository {
   final UserService userService;
 
   UserRepository({required this.userService});
 
-  Future<User?> getUser() async {
+  Future<UserModel?> getUser() async {
     try {
-      MainLogger.logInfo("Try to get page");
+      MainLogger.logInfo("Try to get user");
       return await userService.getUser();
     } catch (e, stackTrace) {
       MainLogger.logError(e, stackTrace);
