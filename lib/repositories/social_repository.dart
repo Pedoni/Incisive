374b14b88fc97a85beeac93c6f8751f8cc54221b
Emanuele Lamagna <emanuele.lamagna@studio.unibo.it>
2026-01-10 10:35:56 +0100

    Added social repository

---
+++ lib/repositories/social_repository.dart
@@ -0,0 +1,29 @@
+import 'package:incisive/log/main_logger.dart';
+import 'package:incisive/models/social_post_model.dart';
+import 'package:incisive/source/remote/social_service.dart';
+
+class SocialRepository {
+  final SocialService socialService;
+
+  SocialRepository({required this.socialService});
+
+  Future<List<SocialPostModel>> getDailyPosts(DateTime dateTime) async {
+    try {
+      MainLogger.logInfo("Try to get daily posts");
+      final list = await socialService.getDailyPosts(date: dateTime);
+      return list
+          .map(
+            (e) => SocialPostModel(
+              content: e['content'],
+              datetime: DateTime.parse(e['datetime']),
+              authorId: e['authorId'],
+              title: e['title'],
+            ),
+          )
+          .toList();
+    } catch (e, stackTrace) {
+      MainLogger.logError(e, stackTrace);
+      rethrow;
+    }
+  }
+}
