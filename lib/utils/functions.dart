a3d4cf2786faece2129099a9b9ede0200b0b2f6c
Emanuele Lamagna <emanuele.lamagna@studio.unibo.it>
2026-01-10 10:28:45 +0100

    Added util functions

--- lib/utils/functions.dart
+++ lib/utils/functions.dart
@@ -2,3 +2,7 @@ String toSqlDate(DateTime d) =>
     "${d.year.toString().padLeft(4, '0')}-"
     "${d.month.toString().padLeft(2, '0')}-"
     "${d.day.toString().padLeft(2, '0')}";
+
+DateTime startOfDay(DateTime date) => DateTime(date.year, date.month, date.day);
+
+DateTime startOfNextDay(DateTime date) => DateTime(date.year, date.month, date.day).add(const Duration(days: 1));
