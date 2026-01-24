dc7689bfd1fe0bab2ccacb10d5d1bd64077860bf
Emanuele Lamagna <emanuele.lamagna@studio.unibo.it>
2026-01-24 11:24:14 +0100

    Fixed webapp on mobile

--- lib/main.dart
+++ lib/main.dart
@@ -2,19 +2,16 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:incisive/app.dart';
-import 'package:incisive/log/app_logger.dart';
+import 'package:incisive/log/app_logger_mobile.dart';
+import 'package:incisive/log/app_logger_web.dart';
 import 'package:incisive/log/main_logger.dart';
-import 'package:incisive/log/mobile_file_logger.dart';
-import 'package:incisive/log/web_logger.dart';
 import 'package:supabase_auth_ui/supabase_auth_ui.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await dotenv.load(fileName: ".env");
 
-  final AppLogger logger = kIsWeb ? WebLogger() : MobileFileLogger();
-
-  MainLogger.init(logger);
+  MainLogger.init(kIsWeb ? WebLogger() : MobileFileLogger());
 
   FlutterError.onError = MainLogger.logFlutterError;
 
